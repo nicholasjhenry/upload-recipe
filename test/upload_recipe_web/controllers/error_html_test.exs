dfb69b032f0ca6f15dc02c7dc995eb20cc1dedcd
Nicholas Henry <nicholas.henry@gmail.com>
2024-09-14 09:34:21 -0400

    Format

--- test/upload_recipe_web/controllers/error_html_test.exs
+++ test/upload_recipe_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule UploadRecipeWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(UploadRecipeWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(UploadRecipeWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
